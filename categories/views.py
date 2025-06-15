d09ae801a3366900b4482201aea162dfc16d1620
Michael Biel <mick.the.linux.geek@hotmail.com>
2025-06-15 16:48:50 -0400

    Commented out unused new_category and edit_category view functions.

--- categories/views.py
+++ categories/views.py
@@ -1,62 +1,61 @@
-from django.contrib import messages
-from django.contrib.auth.decorators import login_required, permission_required
+# from django.contrib import messages
+# from django.contrib.auth.decorators import login_required, permission_required
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.db.models import Count
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import get_object_or_404, render
 
-from .forms import CategoryForm
+# from .forms import CategoryForm
 from .models import Category
 
-
 # Create your views here.
-@login_required
-@permission_required("categories.add_category", raise_exception=True)
-def new_category(request):
-    """
-    View for creating a new category.
-    Only users with the 'add_category' permission can access this view.
-    """
-    if request.method == "POST":
-        form = CategoryForm(request.POST)
-        if form.is_valid():
-            category = form.save()
-            messages.success(
-                request,
-                f'Category "{category.name}" created successfully.',
-            )
-            return redirect("category_list")  # Redirect to category list view
-    else:
-        form = CategoryForm()
-
-    return render(request, "categories/new_category.html", {"form": form})
-
-
-@login_required
-@permission_required("categories.change_category", raise_exception=True)
-def edit_category(request, category_slug):
-    """
-    View for editing an existing category.
-    Only users with the 'change_category' permission can access this view.
-    """
-    category = get_object_or_404(Category, slug=category_slug)
-
-    if request.method == "POST":
-        form = CategoryForm(request.POST, instance=category)
-        if form.is_valid():
-            category = form.save()
-            messages.success(
-                request,
-                f'Category "{category.name}" updated successfully.',
-            )
-            return redirect("category_list")  # Redirect to category list view
-    else:
-        form = CategoryForm(instance=category)
-
-    return render(
-        request,
-        "categories/edit_category.html",
-        {"form": form, "category": category},
-    )
+# @login_required
+# @permission_required("categories.add_category", raise_exception=True)
+# def new_category(request):
+#     """
+#     View for creating a new category.
+#     Only users with the 'add_category' permission can access this view.
+#     """
+#     if request.method == "POST":
+#         form = CategoryForm(request.POST)
+#         if form.is_valid():
+#             category = form.save()
+#             messages.success(
+#                 request,
+#                 f'Category "{category.name}" created successfully.',
+#             )
+#             return redirect("category_list")  # Redirect to category list view
+#     else:
+#         form = CategoryForm()
+#
+#     return render(request, "categories/new_category.html", {"form": form})
+#
+#
+# @login_required
+# @permission_required("categories.change_category", raise_exception=True)
+# def edit_category(request, category_slug):
+#     """
+#     View for editing an existing category.
+#     Only users with the 'change_category' permission can access this view.
+#     """
+#     category = get_object_or_404(Category, slug=category_slug)
+#
+#     if request.method == "POST":
+#         form = CategoryForm(request.POST, instance=category)
+#         if form.is_valid():
+#             category = form.save()
+#             messages.success(
+#                 request,
+#                 f'Category "{category.name}" updated successfully.',
+#             )
+#             return redirect("category_list")  # Redirect to category list view
+#     else:
+#         form = CategoryForm(instance=category)
+#
+#     return render(
+#         request,
+#         "categories/edit_category.html",
+#         {"form": form, "category": category},
+#     )
 
 
 def category_list(request):
