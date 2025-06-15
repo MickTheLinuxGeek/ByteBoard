868752db31ec7c9bd4db35795766899faaa264ac
Michael Biel <mick.the.linux.geek@hotmail.com>
2025-06-15 16:35:30 -0400

    Code reformat via ruff

--- categories/urls.py
+++ categories/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+
 from . import views
 
-app_name = 'categories'
+app_name = "categories"
 
 urlpatterns = [
-    path('', views.category_list, name='category_list'),
+    path("", views.category_list, name="category_list"),
     # This URL pattern will be implemented later for task 4.2
-    path('<slug:category_slug>/', views.topics_by_category, name='topics_by_category'),
+    path("<slug:category_slug>/", views.topics_by_category, name="topics_by_category"),
 ]
